4047fe66f8e3e389ffe770aa8e4eba1344b9e680
Agustin Baza <agustinbaza10@gmail.com>
2023-06-17 02:17:46 -0300

    Termine el maquetado del navbar y footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-dark text-light py-4 text-center ">
+        <p>&copy; Creado por Agustin Baza </p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
---
+++ src/components/common/Menu.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navbar , Container, Nav } from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Menu() {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg" className="p-3">
+      <Container>
+        <Navbar.Brand href="/"> Blog de recetas</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Inicio</Nav.Link>
+            <Nav.Link href="/registro">Contacto</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Menu;
